4e5f42cb2221afa585eafab98d6e407e4239cc9b
kene-zero <zero4321@outlook.com>
2023-11-17 10:29:38 +0800

    新加test目录

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-import os
+import psycopg2
 
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-LOG_PATH = os.path.join(BASE_PATH, "logs")
+conn = psycopg2.connect(database="postgres", user="test1", password="gbase;123", host="192.168.100.100", port="15400")
+cur = conn.cursor()
+cur.execute("""SELECT tablerows('"' ||table_schema || '"."' || table_name || '"') AS CT,
+table_schema || '.' || table_name 
+AS table_full_name
+FROM 
+information_schema.tables
+where table_catalog='ecology_2210'
+ORDER BY
+    tablerows('"' ||table_schema || '"."' || table_name || '"')
+DESC ;""")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
 
 
-def test():
-    pass
 
 
-if __name__ == "__main__":
-    test()
